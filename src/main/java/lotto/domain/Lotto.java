871b0cbc65e429108fd91beddade998e3002b008
wjunseok <wminsoo1@naver.com>
2025-11-03 01:33:51 +0900

    feat(lotto): Lotto 검증 로직 및 당첨 판별 로직 추가
    1.  Lotto 유효성 검증: 번호 개수, 범위(1-45), 중복 검증 로직을 validate 메서드에 통합.
    2.  불변성 및 정렬 보장: 생성자에서 입력 리스트를 방어적 복사 및 정렬 후 `unmodifiableList`로 내부 필드 할당.
    3.  당첨 판별:Lotto.match(bonus,winningNumbers) 메서드로 자신의 당첨 등수(Rank)를 반환하는 로직 추가
    4.  `toString()` 메서드를 오버라이드하여 로또 번호 출력 형식 제공.

---
+++ src/main/java/lotto/domain/Lotto.java
@@ -0,0 +1,74 @@
+package lotto.domain;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class Lotto {
+
+    private static final int LOTTO_SIZE = 6;
+    private static final int MIN_NUMBER = 1;
+    private static final int MAX_NUMBER = 45;
+
+    private final List<Integer> numbers;
+
+    public Lotto(List<Integer> numbers) {
+        validate(numbers);
+
+        List<Integer> numbersCopy = new ArrayList<>(numbers);
+        Collections.sort(numbersCopy);
+        this.numbers = Collections.unmodifiableList(numbersCopy);
+    }
+
+    public Rank match(int bonusNumber, List<Integer> winningNumbers) {
+        int matchCount = 0;
+        for (int number: numbers) {
+            if(winningNumbers.contains(number)) {
+                matchCount++;
+            }
+        }
+
+        boolean matchBonus = winningNumbers.contains(bonusNumber);
+
+        return Rank.of(matchCount, matchBonus);
+    }
+
+    private void validate(List<Integer> numbers) {
+        validateSize(numbers);
+        validateRange(numbers);
+        validateDuplicate(numbers);
+    }
+
+    private void validateSize(List<Integer> numbers) {
+        if (numbers.size() != LOTTO_SIZE) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 " + LOTTO_SIZE + "개여야 합니다.");
+        }
+    }
+
+    private void validateRange(List<Integer> numbers) {
+        for (int number: numbers) {
+            if (number < MIN_NUMBER || number > MAX_NUMBER) {
+                throw new IllegalArgumentException(
+                        "[ERROR] 로또 번호는 " + MIN_NUMBER + "부터 " + MAX_NUMBER + " 사이의 숫자여야 합니다.");
+            }
+        }
+    }
+
+    private void validateDuplicate(List<Integer> numbers) {
+        Set<Integer> uniqueNumbers = new HashSet<>();
+        for (int number: numbers) {
+            if (uniqueNumbers.contains(number)) {
+                throw new IllegalArgumentException("[ERROR] 당첨 번호는 중복될 수 없습니다.");
+            }
+            uniqueNumbers.add(number);
+        }
+    }
+
+    @Override
+    public String toString() {
+        return numbers.toString();
+    }
+
+}
